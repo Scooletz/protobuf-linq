dc7165fb75c7f733cd553e32895318d8b6abe89c
Scooletz <Scooletz@gmail.com>
2013-05-27 23:31:17 +0200

    The root API of the optimized LINQ

---
+++ src/protobuf-linq/IProtobufQueryable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ProtoBuf
+{
+    /// <summary>
+    /// The basic interface for queryable protobuf serialized items, allowing LINQ, not using an <see cref="IQueryable{T}"/> interface.
+    /// </summary>
+    /// <remarks>
+    /// For more information about 'ab'using LINQ, visit: http://bartdesmet.net/blogs/bart/archive/2010/01/01/the-essence-of-linq-minlinq.aspx
+    /// 
+    /// The interface provides a minimal set, allowing some optimizaitons to occur. The two root methods are these provided with <see cref="IWhere{TSource}"/> interface
+    /// and <see cref="ISelect{TSource}"/> interface. All the others can be translated into these two (see <see cref="ProtobufQueryableExtensions"/> for more information). 
+    /// Future optimizations might unwind the rest of methods.
+    /// </remarks>
+    /// <typeparam name="TSource">The root type of the deserialized items. As in standard protobuf.</typeparam>
+    public interface IProtobufQueryable<TSource> : IAny, IOfType, ISelect<TSource>, IWhere<TSource>
+    {
+        IEnumerable<TSource> AsEnumerable();
+    }
+
+    public interface IAny
+    {
+        bool Any();
+    }
+
+    public interface IOfType
+    {
+        IEnumerable<TResult> OfType<TResult>();
+    }
+
+    public interface ISelect<TSource>
+    {
+        IEnumerable<TResult> Select<TResult>(Expression<Func<TSource, TResult>> selector);
+        IEnumerable<TResult> Select<TResult>(Expression<Func<TSource, int, TResult>> selector);
+    }
+
+    public interface IWhere<TSource>
+    {
+        IProtobufQueryable<TSource> Where(Expression<Func<TSource, bool>> predicate);
+        IProtobufQueryable<TSource> Where(Expression<Func<TSource, int, bool>> predicate);
+    }
+}
---
+++ src/protobuf-linq/ProtobufQueryableExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ProtoBuf
+{
+    /// <summary>
+    /// An extension methods translating almost all LINQ methods into calls based on the <see cref="IProtobufQueryable{TSource}"/> interface provided methods.
+    /// </summary>
+    /// <remarks>
+    /// This gives a full-blown LINQ for items deserialized with protobufs.
+    /// </remarks>
+    public static class ProtobufQueryableExtensions
+    {
+        public static TSource ElementAtOrDefault<TSource>(this IProtobufQueryable<TSource> source, int index)
+        {
+            return source.Skip(index).AsEnumerable().FirstOrDefault();
+        }
+
+        public static TSource ElementAt<TSource>(this IProtobufQueryable<TSource> source, int index)
+        {
+            return source.Skip(index).AsEnumerable().First();
+        }
+
+        public static double Average<TSource>(this IProtobufQueryable<TSource> source, Expression<Func<TSource, double>> selector)
+        {
+            return source.Select(selector).Average();
+        }
+
+        public static double? Average<TSource>(this IProtobufQueryable<TSource> source, Expression<Func<TSource, double?>> selector)
+        {
+            return source.Select(selector).Average();
+        }
+
+        public static Decimal Average<TSource>(this IProtobufQueryable<TSource> source, Expression<Func<TSource, Decimal>> selector)
+        {
+            return source.Select(selector).Average();
+        }
+
+        public static Decimal? Average<TSource>(this IProtobufQueryable<TSource> source, Expression<Func<TSource, Decimal?>> selector)
+        {
+            return source.Select(selector).Average();
+        }
+
+        public static TAccumulate Aggregate<TSource, TAccumulate>(this IProtobufQueryable<TSource> source, TAccumulate seed, Expression<Func<TAccumulate, TSource, TAccumulate>> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TResult Aggregate<TSource, TAccumulate, TResult>(this IProtobufQueryable<TSource> source,
+            TAccumulate seed,
+            Expression
+                <Func<TAccumulate, TSource, TAccumulate>>
+                func,
+            Expression<Func<TAccumulate, TResult>> selector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IProtobufQueryable<TSource> Skip<TSource>(this IProtobufQueryable<TSource> source, int i)
+        {
+            return source.Where((e, index) => index >= i);
+        }
+
+        public static bool Any<TSource>(this IProtobufQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
+        {
+            return source.Where(predicate).Any();
+        }
+        
+        public static bool All<TSource>(this IProtobufQueryable<TSource> source, Expression<Func<TSource, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
